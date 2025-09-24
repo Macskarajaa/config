32ec6d77538c671b09eb19b8800cf3f3901e4cb6
unknown <ftamas2006@gmail.com>
2025-09-24 11:42:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+import express from "express"
+import cors from "cors"
+import { configDB } from "./configDB.js"
+import mysql from "mysql2/promise"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+const PORT = 8000
+
+let connection
+
+try{
+    connection = await mysql.createConnection(configDB)
+}catch(error){
+    console.log(console.error())
+}
+
+
+app.get("/todos", async(req, res)=>{
+    try {
+        const sql = "SELECT * FROM todolist ORDER BY timestamp DESC"
+        const [rows, fields]=await connection.execute(sql)
+        //console.log(rows);
+        //console.log(fields)
+        res.status(200).send(rows)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/todos", async(req, res)=>{
+    const {task} = req.body
+    if(!task) return response.json({msg:"hiányos adat"})
+        try {
+            const sql="insert into todolist (task) values (?)"
+            const values=[task]
+            const [result]= await connection.execute(sql,values)
+            console.log(result);
+            res.status(201).json({msg:"Sikeres hozzáadás!"})
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+})
+
+app.delete("/todos/:id", async(req,res)=>{
+    const {id} = req.params
+    try {
+        const sql="DELETE FROM todolist WHERE id=?"
+        const values = [id]
+        const [rows] = await connection.execute(sql,values)
+        console.log(rows.affectedRows);
+        if(rows.affectedRows==0) return response.json({msg:"nincs mit törölni"})
+        res.status(200).json({msg:"sikeres törlés"})
+        
+    } catch (error) {
+        console.log(error)
+    }
+   
+})
+
+
+app.patch("/todos/:id", async (req,res)=>{
+    const {id} = req.params
+    try {
+        const sql="update todolist set completed = not completed where id=?"
+        const values =  [id]
+        const [rows] = await connection.execute(sql,values)
+        if(rows.affectedRows==0) return response.json({msg:"nincs több sor"})
+        res.json({msg:"sikeres hozzáadás"})
+    }
+        catch (error) {
+        console.log(error)
+    }
+        
+    }
+})
+
+
+app.listen(PORT, ()=>console.log(`server listening on ${PORT}`))
+
